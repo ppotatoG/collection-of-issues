37f312e82240e8ffa9865908ed58c786b03842fe
ppotatoG <thgus2270@gmail.com>
2022-09-25 14:02:46 +0900

    feat: delete repo

--- src/components/Issues.tsx
+++ src/components/Issues.tsx
@@ -8,7 +8,7 @@ import { FaRegDotCircle, FaRegCommentAlt } from "react-icons/fa";
 import '../styles/issues.scss';
 
 const Issues = () => {
-    const [issues, setIssues] = useState<issue[] | null>([]);
+    const [issues, setIssues] = useState<issue[] | null>(JSON.parse(localStorage.getItem('viewIssue') || '{}'));
     const [loading, setLoading] = useState<boolean>(true);
 
     const repos = JSON.parse(localStorage.getItem('viewIssue') || '{}');
@@ -16,14 +16,25 @@ const Issues = () => {
 
     const fetchRepos = () => {
         setLoading(true);
+        setIssues([]);
 
         axios.get(`${repos}/issues`)
             .then((res) => {
-                setIssues((prevIssue : any | null) => [...prevIssue, ...res.data]);
+                setIssues(res.data);
             }).catch((error) => {
                 console.log(error.message)
             }).then(() => setLoading(false));
     };
+
+    const viewIssueArr : string[] = [];
+
+    const deleteRepo = (e : any, repoUrl : string) => {
+        e.preventDefault();
+
+        localStorage.setItem('viewIssue', JSON.stringify(viewIssueArr));
+        viewIssueArr.filter((v : string) => v !== repoUrl);
+        localStorage.setItem('viewIssue', JSON.stringify(viewIssueArr));
+    }
 
     useEffect(() => {
         fetchRepos();
@@ -36,13 +47,26 @@ const Issues = () => {
             {
                 issues
                 ? <ul className="issues">
-                    <li>{repos}</li>
+                    <li>
+                        {
+                            repos.map((repo : any, idx : number) => {
+                                const [userName, repoName] = repo.split('https://api.github.com/repos/')[1].split('/');
+                                return (
+                                    <button
+                                        key={idx}
+                                        onClick={(e) => deleteRepo(e, repo)}
+                                    >
+                                        {userName} {repoName}
+                                    </button>
+                                )
+                            })
+                        }
+                    </li>
                     {
                         Object.values(issues).map((val : issue, idx: number) => {
                             return (
                                 <li
                                     className="issues__item"
-                                    onClick={() => window.open(val.html_url)}
                                     key={idx}
                                 >
                                     <FaRegDotCircle />
@@ -55,6 +79,7 @@ const Issues = () => {
                                         val.comments !== 0 &&
                                         <p className="comments"><FaRegCommentAlt/>{val.comments}</p>
                                     }
+                                    <a href={val.html_url} target="_blank" rel="author"></a>
                                 </li>
                             )
                         })
