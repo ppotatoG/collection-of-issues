caf7f371073f7ad93ee6c3ef2abf8b84d43cbd6c
ppotatoG <thgus2270@gmail.com>
2022-11-24 15:18:07 +0900

    refactor: axios useEffect

--- src/components/Issue/index.tsx
+++ src/components/Issue/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useCallback} from "react";
 import axios from 'axios';
 import Loading from 'components/loading';
 
@@ -14,22 +14,25 @@ const Issues = () => {
     const [repos, setRepos] = useState<string[]>(JSON.parse(localStorage.getItem('viewIssue') || '[]'));
     const [loading, setLoading] = useState<boolean>(false);
 
-    const fetchIssues = async () => {
+    const fetchIssues = useCallback(async () => {
         try {
             setLoading(true);
             if (Object.keys(repos).length) {
                 setIssues([]);
-                const res = await axios.all(repos.map((repo: string) => axios.get(`${repo}/issues`)))
-                res.map(item => {
-                    return setIssues((prevIssue: IssuesType[]) => [...prevIssue, ...item.data]);
-                })
+                const res = await axios.all(repos.map((repo: string) => axios.get(`${repo}/issues`)));
+                res.map(item => setIssues((prevIssue: IssuesType[]) => [...prevIssue, ...item.data]));
             }
         } catch (e) {
-            console.error(`${e} fetchIssues CALL FAILURE`)
+            console.error(`${e} fetchIssues CALL FAILURE`);
         } finally {
-            setLoading(false)
+            setLoading(false);
         }
-    };
+    }, [repos]);
+
+    useEffect(() => {
+        localStorage.setItem('viewIssue', JSON.stringify(repos));
+        fetchIssues().then();
+    }, [fetchIssues, repos]);
 
     const ViewIssues = (): JSX.Element => {
         if (repos.length === 0) return <BlankContent blankType={'저장된 레포지토리가 없어요'}/>
@@ -42,11 +45,6 @@ const Issues = () => {
            />
         )
     }
-
-    useEffect(() => {
-        localStorage.setItem('viewIssue', JSON.stringify(repos));
-        fetchIssues();
-    }, [repos]);
 
     return (
         <div>
